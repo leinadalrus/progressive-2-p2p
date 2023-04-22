28da7ad1f2ce2cbabab0dc1edff6878ab74b3203
Daniel Surla <s3729065@student.rmit.edu.au>
2023-04-22 06:28:01 +0100

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,39 @@
-trait ArbitraryNodeNetwork {
+use bevy::{
+    math::Vec3Swizzles, prelude::*, sprite::MaterialMesh2dBundle,
+    DefaultPlugins,
+};
+
+pub enum ModulusPolling {
+    PreviousKey { is_remainder_previous_key: bool },
+    EntityRemoved,
+    NotPresent,
+}
+
+pub trait ModulusPoller {
+    fn is_modulus_previous_key(i_th: u32) -> bool;
+    fn previous_key_callback() -> u32;
+}
+
+pub trait ArbitraryNodeNetwork {
     fn join(finger: FingerTable);
-    fn update_finger_table(finger: FingerTable, successor: u32, predecessor: u32);
+    fn update_finger_table(
+        finger: FingerTable, successor: u32, predecessor: u32,
+    );
     fn notify(finger: FingerTable, node_id1: u32);
     fn stabilize(finger: FingerTable);
     fn fix_fingers(finger: FingerTable);
     fn check_predecessor(finger: FingerTable);
 }
 
-trait ModulusPoller {
-    fn is_modulus_previous_key(i_th: u32) -> bool;
-    fn previous_key_callback() -> u32;
-}
-
 #[derive(Debug, Clone, Copy)]
-struct ChordDistributedHashTable {
+pub struct DistributedHashTable {
     predecessor: u32,
     successor: u32,
     hashkey: u32,
     size: usize,
 }
 
-impl ChordDistributedHashTable {
+impl DistributedHashTable {
     pub fn avoid_collision(avoid_collision_m: u32) -> u32 {
         return u32::pow(2, avoid_collision_m) - 1;
     }
@@ -31,27 +44,28 @@ impl ChordDistributedHashTable {
 }
 
 #[derive(Debug, Clone, Copy)]
-struct FingerTable {
+pub struct FingerTable {
     interval_matrix: [[u32; 2]; 10],
     keys: [u32; 10],
-    chord: ChordDistributedHashTable,
+    dht: DistributedHashTable,
 }
 
 impl FingerTable {
     fn starting_node(self: FingerTable) -> u32 {
-        return 14 * self.chord.predecessor + 67;
+        return 14 * self.dht.predecessor + 67;
     }
 
     fn interval(self: FingerTable) -> u32 {
-        let new_key =
-            ChordDistributedHashTable::recorded_arc(self.chord.hashkey) + self.chord.successor;
+        let new_key = DistributedHashTable::recorded_arc(self.dht.hashkey)
+            + self.dht.successor;
         self.keys[new_key as usize];
-        return new_key - self.chord.predecessor;
+        return new_key - self.dht.predecessor;
     }
 
     fn find_successor(mut self: FingerTable, mut node_pred: u32) -> u32 {
-        let successor_k =
-            node_pred + u32::pow(2, self.keys[node_pred as usize - 1]) * u32::pow(node_pred, 2);
+        let successor_k = node_pred
+            + u32::pow(2, self.keys[node_pred as usize - 1])
+                * u32::pow(node_pred, 2);
 
         for i in 0..2 {
             for j in 0..10 {
@@ -60,7 +74,8 @@ impl FingerTable {
             }
 
             if node_pred != node_pred / successor_k {
-                node_pred = FingerTable::closest_preceding_finger(self, node_pred);
+                node_pred =
+                    FingerTable::closest_preceding_finger(self, node_pred);
             }
         }
 
@@ -70,7 +85,7 @@ impl FingerTable {
     fn find_predecessor(self: FingerTable, node_succ: u32) -> u32 {
         let mut i_th = FingerTable::starting_node(self);
 
-        if i_th != (i_th / self.chord.successor) {
+        if i_th != (i_th / self.dht.successor) {
             i_th = FingerTable::closest_preceding_finger(self, node_succ);
         }
 
@@ -93,13 +108,15 @@ impl ArbitraryNodeNetwork for FingerTable {
     fn join(mut finger: FingerTable) {
         FingerTable::find_predecessor(
             finger,
-            ChordDistributedHashTable::recorded_arc(finger.chord.predecessor),
+            DistributedHashTable::recorded_arc(finger.dht.predecessor),
         );
-        finger.chord.predecessor = 0;
-        FingerTable::find_successor(finger, finger.chord.predecessor);
+        finger.dht.predecessor = 0;
+        FingerTable::find_successor(finger, finger.dht.predecessor);
     }
 
-    fn update_finger_table(mut finger: FingerTable, successor: u32, predecessor: u32) {
+    fn update_finger_table(
+        mut finger: FingerTable, successor: u32, predecessor: u32,
+    ) {
         let node = FingerTable::starting_node(finger);
         let interval = finger.keys[FingerTable::starting_node(finger) as usize];
 
@@ -111,25 +128,29 @@ impl ArbitraryNodeNetwork for FingerTable {
     }
 
     fn notify(mut finger: FingerTable, node_id1: u32) {
-        if finger.chord.predecessor == 0
-            || node_id1 > finger.chord.predecessor && node_id1 < FingerTable::starting_node(finger)
+        if finger.dht.predecessor == 0
+            || node_id1 > finger.dht.predecessor
+                && node_id1 < FingerTable::starting_node(finger)
         {
-            finger.chord.predecessor = node_id1;
+            finger.dht.predecessor = node_id1;
         }
     }
 
     fn stabilize(mut finger: FingerTable) {
-        let x = finger.chord.predecessor;
-        if x > finger.chord.predecessor && x < finger.chord.successor {
-            finger.chord.successor = x;
+        let x = finger.dht.predecessor;
+        if x > finger.dht.predecessor && x < finger.dht.successor {
+            finger.dht.successor = x;
         }
 
-        <FingerTable as ArbitraryNodeNetwork>::notify(finger, finger.chord.predecessor);
+        <FingerTable as ArbitraryNodeNetwork>::notify(
+            finger,
+            finger.dht.predecessor,
+        );
     }
 
     fn fix_fingers(finger: FingerTable) {
-        let mut next = finger.chord.successor + 1;
-        if next > ChordDistributedHashTable::avoid_collision(1) {
+        let mut next = finger.dht.successor + 1;
+        if next > DistributedHashTable::avoid_collision(1) {
             next = 1;
         }
 
@@ -137,13 +158,108 @@ impl ArbitraryNodeNetwork for FingerTable {
     }
 
     fn check_predecessor(mut finger: FingerTable) {
-        if finger.chord.predecessor == 0 {
-            finger.chord.predecessor = 0;
+        if finger.dht.predecessor == 0 {
+            finger.dht.predecessor = 0;
         }
     }
 }
 
-fn main() {
+#[derive(Component)]
+pub struct ArbitraryNode {
+    rotation_speed: f32,
+}
+#[derive(Component)]
+pub struct SnapsTowardsChord;
+#[derive(Component)]
+pub struct RotatesAroundChord {
+    pub rotation_speed: f32,
+}
+
+fn startup_setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn bundle_material_mesh(
+    mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+        material: materials.add(ColorMaterial::from(Color::PURPLE)),
+        transform: Transform::from_translation(Vec3::new(-150., 0., 0.)),
+        ..default()
+    });
+}
+
+fn load_entity_handle(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let chord_shape_handle = asset_server.load("assets/images/circle.png");
+    let arbitrary_node_handle = asset_server.load("assets/images/circle.png");
+
+    commands.spawn((
+        SpriteBundle {
+            texture: chord_shape_handle,
+            ..default()
+        },
+        ArbitraryNode {
+            rotation_speed: f32::to_radians(360.0),
+        },
+    ));
+
+    commands.spawn((
+        SpriteBundle {
+            texture: arbitrary_node_handle,
+            ..default()
+        },
+        RotatesAroundChord {
+            rotation_speed: f32::to_radians(360.0),
+        },
+    ));
+}
+
+fn entity_rotation_system(mut query: Query<(&ArbitraryNode, &mut Transform)>) {
+    let delta_time = 1.0 / 60.0;
+    let chord_boundary = Vec2::new(1.0, 1.0);
+
+    let (arbitrary_node, mut transform) = query.single_mut();
+    let rotation_factor = 0.0;
+
+    transform
+        .rotate_z(rotation_factor * arbitrary_node.rotation_speed * delta_time);
+
+    let varied_chord_boundary = Vec3::from((chord_boundary / 2.0, 0.0));
+
+    transform.translation = transform
+        .translation
+        .min(varied_chord_boundary)
+        .max(-varied_chord_boundary);
+}
+
+fn transform_entity_query(
+    mut query: Query<
+        &mut Transform,
+        (With<SnapsTowardsChord>, Without<ArbitraryNode>),
+    >,
+    chord_query: Query<&Transform, With<ArbitraryNode>>,
+) {
+    let node_properties = chord_query.single();
+    let node_translation = node_properties.translation.xy();
+
+    for mut arbitrary_node in &mut query {
+        let within_towards_centre =
+            (node_translation - arbitrary_node.translation.xy()).normalize();
+        let nodes_rotate_within_chord =
+            Quat::from_rotation_arc(Vec3::Y, within_towards_centre.extend(0.0));
+        arbitrary_node.rotation = nodes_rotate_within_chord;
+    }
+}
+
+fn execute_arbitrary_network_command(
+    mut query: Query<
+        (&RotatesAroundChord, &mut Transform),
+        Without<ArbitraryNode>,
+    >,
+    arbitrary_node: Query<&Transform, With<ArbitraryNode>>,
+) {
     let vector_matrix: [[u32; 2]; 10] = [
         [1, 0],
         [2, 85],
@@ -157,7 +273,7 @@ fn main() {
         [10, 451],
     ];
     let array_keys: [u32; 10] = [0, 85, 133, 182, 210, 245, 279, 324, 395, 451];
-    let chord = ChordDistributedHashTable {
+    let dht = DistributedHashTable {
         predecessor: 0,
         successor: 1,
         hashkey: 85,
@@ -166,7 +282,7 @@ fn main() {
     let finger = FingerTable {
         interval_matrix: vector_matrix,
         keys: array_keys,
-        chord: chord,
+        dht,
     };
 
     finger.starting_node();
@@ -177,10 +293,26 @@ fn main() {
 
     loop {
         <FingerTable as ArbitraryNodeNetwork>::join(finger);
-        <FingerTable as ArbitraryNodeNetwork>::update_finger_table(finger, successor, predecessor);
+        <FingerTable as ArbitraryNodeNetwork>::update_finger_table(
+            finger,
+            successor,
+            predecessor,
+        );
         <FingerTable as ArbitraryNodeNetwork>::check_predecessor(finger);
         <FingerTable as ArbitraryNodeNetwork>::notify(finger, interval);
         <FingerTable as ArbitraryNodeNetwork>::stabilize(finger);
         <FingerTable as ArbitraryNodeNetwork>::fix_fingers(finger);
     }
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(startup_setup)
+        .add_system(bundle_material_mesh)
+        .add_system(load_entity_handle)
+        .add_system(entity_rotation_system)
+        .add_system(transform_entity_query)
+        .add_system(execute_arbitrary_network_command)
+        .run();
 }
